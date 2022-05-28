e32c294162b827b206d3aa5669917f308c09e007
J-Appleg8 <84997748+J-Appleg8@users.noreply.github.com>
2022-05-27 18:18:57 -0700

    Finished food app section

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import classes from './MealItemForm.module.css';
 import Input from '../../UI/Input';
 
+// We are able to pass amountInputRef into <Input because in the Input component
+// we wrapped the component with React.forwardRef()
 const MealItemForm = props => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  const amountInputRef = useRef();
+
+  const submitHandler = event => {
+    event.preventDefault();
+    // always use .current on refs that are created with useRef()
+    const enteredAmount = amountInputRef.current.value;
+    // converts the 'string' number into a 'number' number
+    const enteredAmountNumber = +enteredAmount;
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
+    props.onAddToCart(enteredAmountNumber);
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label="Amount"
         input={{
           id: 'amount_' + props.id,
@@ -17,6 +41,7 @@ const MealItemForm = props => {
         }}
       />
       <button>+ Add</button>
+      {!amountIsValid && <p>Please enter a valid amount (1-5)</p>}
     </form>
   );
 };
---
+++ src/store/CartProvider.js
@@ -0,0 +1,121 @@
+import React, { useReducer } from 'react';
+import CartContext from './cart-context';
+
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+////////////////////////////////////////////////////////////////////////////////
+// state is the last state snapshot of the state managed by the reducer
+// - action is dispatched by you, and you then have to return a new state snapshot
+const cartReducer = (state, action) => {
+  ////////////////////////////////////////
+  // When cartReducer is called by dispatchCartAction in CartProvider below
+  // - We get the item that should be added
+  // - Check if that item is already in the cart -> if so update existing item
+  // - If not then add new item
+  if (action.type === 'ADD') {
+    // Checks if an item is already in the cart
+    // Returns the index number of the item if it exists
+    // Updates the total amount of items in the cart
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+
+    const existingCartItemIndex = state.items.findIndex(
+      item => item.id === action.item.id
+    );
+
+    const existingCartItem = state.items[existingCartItemIndex];
+    let updatedItems;
+
+    // Either adds new items or updates existing item amounts
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+      // Picks old item in index and updates it with the new item
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      // doesn't edit existing array, but adds new item and returns a new array
+      // - Important because we want to return a new State array
+      updatedItems = state.items.concat(action.item);
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+
+  if (action.type === 'REMOVE') {
+    const existingCartItemIndex = state.items.findIndex(
+      item => item.id === action.id
+    );
+    const existingItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+    let updatedItems;
+    // If there is only 1, then we want to remove it from the cart
+    if (existingItem.amount === 1) {
+      // Returns a new array filtered from conditions as true/false
+      // Pass function in filter() that's executed on each item in array
+      // - only keeps items that are true
+      updatedItems = state.items.filter(item => item.id !== action.id);
+    } else {
+      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+
+  return defaultCartState;
+};
+
+////////////////////////////////////////////////////////////////////////////////
+// Calling add item in MealItemForm.js
+const CartProvider = props => {
+  // State snapshot & Function that allows you to dispatch an action to the reducer
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+  // Add item to cart handler
+  const addItemToCartHandler = item => {
+    // action is typically an object which as some property that allows you to
+    // identify that action inside of the reducer function
+    dispatchCartAction({
+      type: 'ADD',
+      item: item,
+    });
+  };
+
+  // Remove item to cart handler
+  const removeItemToCartHandler = id => {
+    dispatchCartAction({
+      type: 'REMOVE',
+      id: id,
+    });
+  };
+
+  //
+  const cartContext = {
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemToCartHandler,
+  };
+
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartProvider;
